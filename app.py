9ffc522043bd3c59bccc5139457a3e076d55609d
kfru5515 <kfru5515@naver.com>
2025-07-03 17:41:02 +0900

    주식 상관계수

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from blueprints.auth import auth_bp
 import FinanceDataReader as fdr
 import pandas as pd
 from dotenv import load_dotenv
+import os
 
 from pytz import timezone # 한국시간 사용 
 
@@ -120,8 +121,25 @@ def index():
         today=today_str
     )
 
+#그므
+def get_cpi_data_from_bok():
+    api_key = os.getenv("ECOS_API_KEY")
+    if not api_key:
+        raise ValueError("ECOS_API_KEY가 .env에서 로드되지 않았습니다.")
 
+    url = (
+        f"http://ecos.bok.or.kr/api/StatisticSearch/{api_key}/json/kr/1/100/"
+        f"901Y014/2020/2025/0000001/M"
+    )
 
+    response = requests.get(url)
+    data = response.json()
+
+    rows = data['StatisticSearch']['row']
+    cpi_dates = [f"{r['TIME'][:4]}-{r['TIME'][4:]}" for r in rows]
+    cpi_values = [float(r['DATA_VALUE']) for r in rows]
+
+    return cpi_dates[-12:], cpi_values[-12:]
     
 def add_trading_value(stock_list):
     for item in stock_list:
