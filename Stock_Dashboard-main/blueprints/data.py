f7df43b2179ff238182cfa04105a9bc6a26ce3fd
kfru5515 <kfru5515@naver.com>
2025-07-29 10:05:43 +0900

    removed junk
    -

--- Stock_Dashboard-main/blueprints/data.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-data_bp = Blueprint('data', __name__, url_prefix='/data')
-
-@data_bp.route('/')
-def data():
-    return render_template('data.html')
--- Stock_Dashboard-main/maps/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-maps_bp = Blueprint('templates', __name__, template_folder='templates')
-
-@maps_bp.route('/maps')
-def maps():
-    return render_template('maps.html')
